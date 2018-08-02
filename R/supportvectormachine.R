6c4db713ca87f45c7ec285d20dba9c5dba3475d6
tejasavkhattar <tejasav1997@gmail.com>
2018-08-03 00:50:46 +0530

    svm

---
+++ R/supportvectormachine.R
@@ -0,0 +1,13 @@
+supportvectormachine <- function(data, formula, formula2, k, var_x, value1, var_y, value2){
+  library(jsonlite)
+  data <- fromJSON(data)
+  data <- na.omit(data)
+  library(e1071)
+  mymodel <- svm(formula, data=data, kernel=k, scale= FALSE)
+  list2 <-list()
+  list2$var_x <- value1
+  list2$var_y <- value2
+  names(list2) <- c(var_x, var_y)
+  plot(mymodel, data, formula2, slice=list2)
+}
+
