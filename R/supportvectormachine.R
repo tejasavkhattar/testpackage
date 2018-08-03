65f33fcfc8403175a96b9a1b94e2f4a1eaa0c2de
tejasavkhattar <tejasav1997@gmail.com>
2018-08-03 10:37:13 +0530

    svm

--- R/supportvectormachine.R
+++ R/supportvectormachine.R
@@ -9,13 +9,15 @@ supportvectormachine <- function(dat, formul, formula2, k, var_x, value1, var_y,
   library(jsonlite)
   dat <- fromJSON(dat)
   dat <- na.omit(dat)
+
   library(e1071)
-  mymodel <- svm(formula=formul, data=dat, kernel=k, type="C-classification", scale=FALSE, y=NULL, x=NULL)
+  mymodel <- svm(formul, data=dat, kernel=k, scale= FALSE)
   list2 <-list()
   list2$var_x <- value1
   list2$var_y <- value2
   names(list2) <- c(var_x, var_y)
-  plot(mymodel, dat, formula2, slice=list2)
-}
 
+  plot(mymodel, dat, formula2, slice=list2)
+
+}
 
