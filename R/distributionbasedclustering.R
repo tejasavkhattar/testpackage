1fdb246f5137bf8a8eba3212422cd2d8137f8885
tejasavkhattar <tejasav1997@gmail.com>
2018-08-04 21:00:44 +0530

    Distribution

---
+++ R/distributionbasedclustering.R
@@ -0,0 +1,12 @@
+distributionbasedclustering <- function(data, classify) {
+  library(jsonlite)
+  data <- fromJSON(data)
+  cl <- data[,grep(classify, names(data))]
+  X <- data[,-grep(classify, names(data))]
+  clPairs(X, cl)
+  BIC <- mclustBIC(X)
+  mod1 <- Mclust(X, x = BIC)
+  plot(mod1, what = "classification")
+}
+
+
