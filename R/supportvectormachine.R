51882497ad8370bb9ae25114e95b217f40e79119
tejasavkhattar <tejasav1997@gmail.com>
2018-08-05 18:06:03 +0530

    svm

--- R/supportvectormachine.R
+++ R/supportvectormachine.R
@@ -1,11 +1,12 @@
 supportvectormachine <- function(dat, formul, formula2, new, k, var_x, value1, var_y, value2) {
   library(jsonlite)
   dat <- fromJSON(dat)
+  dat <- na.omit(dat)
   .formul <- reformulate(termlabels = c('.'), response=formul)
   .formula2 <- reformulate(termlabels = c(new), response=formula2)
   dat <- as.data.frame(dat)
   library(e1071)
-  mymodel <- svm(.formul, data=dat, kernel=k,type="C-classification")
+  mymodel <- svm(.formul, data=dat, kernel=k,type="C-classification", scale = FALSE)
   list2 <-list()
   list2$var_x <- value1
   list2$var_y <- value2
@@ -13,6 +14,6 @@ supportvectormachine <- function(dat, formul, formula2, new, k, var_x, value1, v
   plot(mymodel, dat, .formula2, slice=list2)
 }
 
-#supportvectormachine(iris,"Species", "Petal.Length", "Petal.Width", "polynomial", "Sepal.Width", 3, "Sepal.Length", 4)
+#supportvectormachine(iris,"Species", "Petal.Length", "Petal.Width", "radial", "Sepal.Width", 3, "Sepal.Length", 4)
 
 
