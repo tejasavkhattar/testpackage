bf74437616376afadd40b776a2753cd0eb384f55
tejasavkhattar <tejasav1997@gmail.com>
2018-08-05 17:50:08 +0530

    svm

--- R/supportvectormachine.R
+++ R/supportvectormachine.R
@@ -1,4 +1,6 @@
 supportvectormachine <- function(dat, formul, formula2, new, k, var_x, value1, var_y, value2) {
+  library(jsonlite)
+  dat <- fromJSON(dat)
   .formul <- reformulate(termlabels = c('.'), response=formul)
   .formula2 <- reformulate(termlabels = c(new), response=formula2)
   dat <- as.data.frame(dat)
@@ -11,6 +13,6 @@ supportvectormachine <- function(dat, formul, formula2, new, k, var_x, value1, v
   plot(mymodel, dat, .formula2, slice=list2)
 }
 
-supportvectormachine(iris,"Species", "Petal.Length", "Petal.Width", "polynomial", "Sepal.Width", 3, "Sepal.Length", 4)
+#supportvectormachine(iris,"Species", "Petal.Length", "Petal.Width", "polynomial", "Sepal.Width", 3, "Sepal.Length", 4)
 
 
