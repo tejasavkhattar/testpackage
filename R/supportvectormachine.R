fee6fe31375c05ef6764a198cc4fe9b07a477f92
tejasavkhattar <tejasav1997@gmail.com>
2018-08-05 18:32:49 +0530

    svm

--- R/supportvectormachine.R
+++ R/supportvectormachine.R
@@ -1,17 +1,20 @@
 supportvectormachine <- function(dat, formul, formula2, new, k, var_x, value1, var_y, value2) {
   library(jsonlite)
-  dat <- fromJSON(dat)
-  dat <- na.omit(dat)
+  data <- fromJSON(data)
+  data <- na.omit(data)
   .formul <- reformulate(termlabels = c('.'), response=formul)
   .formula2 <- reformulate(termlabels = c(new), response=formula2)
   dat <- as.data.frame(dat)
   library(e1071)
   mymodel <- svm(.formul, data=dat, kernel=k,type="C-classification", scale = FALSE)
+  summary(mymodel)
   list2 <-list()
   list2$var_x <- value1
   list2$var_y <- value2
   names(list2) <- c(var_x, var_y)
-  plot(mymodel, dat, .formula2, slice=list2)
+  plot(mymodel, dat, .formula2, slice=list2, symbolPalette = rainbow(3),
+       color.palette = terrain.colors)
+  summary(mymodel)
 }
 
 #supportvectormachine(iris,"Species", "Petal.Length", "Petal.Width", "radial", "Sepal.Width", 3, "Sepal.Length", 4)
